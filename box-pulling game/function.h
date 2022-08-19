2262e08d80a41e2f67e79a0d9a2251cbc9cc39b7
Gik-op <73054589+Gik-op@users.noreply.github.com>
2022-08-19 15:32:13 +0800

    Update function.h

--- box-pulling game/function.h
+++ box-pulling game/function.h
@@ -10,6 +10,7 @@ extern int BOX;
 
 /* characters need to print, 'W' for Wall, '-' for blank space, 'P' for Player */
 extern char symbols[];
+enum graph {wall=1,in=2,box=3,aim=4,person=5,get=6,person_in_aim=7,person_in_get=8,box_in_get=9};
 
 /* four moving vectors */
 extern int DIR[5][2];
@@ -25,11 +26,14 @@ extern int DIR[5][2];
 #define RIGHT 3
 #define INPLACE 4
 
+void initializeMap(int map[LENGTH][WIDTH],FILE *fp);
 void playerPosition(int map[LENGTH][WIDTH],int player[2]);
-void playerFinal(int map[LENGTH][WIDTH],int player[2]);
-void printMap(int map[LENGTH][WIDTH], int player[2]);
-int validMove(int map[LENGTH][WIDTH], int player[2], int direction);
-int validBox(int map[LENGTH][WIDTH], int player[2], int direction);
-int number(int map[LENGTH][WIDTH],int n);
+void playerFinal(int map[LENGTH][WIDTH],const int player[2]);
+void playerMove(int map[LENGTH][WIDTH],int player[2],int direction);
+void printMap(int map[LENGTH][WIDTH],const int player[2]);
+void storeMap(int map[LENGTH][WIDTH], int player[2],FILE *fs);
+int validMove(const int map[LENGTH][WIDTH],const int player[2], int direction);
+int validBox(const int map[LENGTH][WIDTH],const int player[2], int direction);
+int number(int map[LENGTH][WIDTH],int figure);
 
 #endif //BOXFINAL_FUNCTION_H
